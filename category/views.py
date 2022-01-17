ae057eec4a68dd9e9fa96fe9a5feb113bec987d1
munyolec@gmail.com <munyolec@gmail.com>
2022-01-17 15:25:10 +0300

    added category page

--- category/views.py
+++ category/views.py
@@ -6,11 +6,17 @@ from products.models import Product
 
 # Create your views here
 
-
 def category(request, category_id):
+    categories = Category.objects.all()
     category = get_object_or_404(Category, pk=category_id)
-    context = {
-        'category': category
-    }
+    category_products = Product.objects.all().filter(category_id=category)
+    
 
-    return render(request, 'categories/category.html', context)
+    context = {
+        'categories':categories,
+        'category': category,
+        'category_products': category_products
+
+    }
+    return render(request, 'category/category.html', context)
+
